054cb79a4141dcf0a3da6c00888fee3c413fb4a6
Luis Saucedo <luisrubalcava17@gmail.com>
2023-02-03 17:44:48 -0600

    fix: codigo js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,31 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = "https://api.github.com";
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+// faltaba un punto para seleccionar clase
+const $n = document.querySelector(".name");
+// se estaba usando el numeral en lugar de punto
+const $b = document.querySelector(".blog");
+const $l = document.querySelector(".location");
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+// Para usar await hay que hacer la función asíncrona
+async function displayUser(username) {
+  $n.textContent = "cargando...";
   const response = await fetch(`${usersEndpoint}/${username}`);
+  // Se hace llamada al método json para extraer datos
+  const data = await response.json();
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  // En las template strings se usó apostrofe en lugar de acento grave
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
-  console.log('OH NO!');
+  console.log("OH NO!");
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  // Le faltaba el $ a la variable n
+  $n.innerHTML = `Algo salió mal: ${err}`;
 }
 
+
 displayUser('stolinski').catch(handleError);
+displayUser("defunkt").catch(handleError);
